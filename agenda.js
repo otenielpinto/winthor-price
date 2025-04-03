6de6ce5cbb222bdaeb863b33828563d312f9ad2a
otenielpinto <oteniel.pinto@gmail.com>
2025-04-03 16:59:23 -0300

    Refactor agenda.js by cleaning up commented-out code and improving readability

--- agenda.js
+++ agenda.js
@@ -2,26 +2,22 @@ const db = require("./config/db");
 const nodeSchedule = require("node-schedule");
 const priceRepository = require("./repository/priceRepository");
 const lib = require("./utils/lib");
-const { exitOnError } = require("winston");
-
 
 async function init() {
-  
   await priceRepository.SyncPricesAllTenants();
   await priceRepository.updatePricesTiny();
-  console.log("Fim da leitura as " + new Date().toLocaleString());  
-  
-//   try {
-//     const time = 60*24; //tempo em minutos
-//     const job = nodeSchedule.scheduleJob(`*/${time} * * * *`, async () => {
-//       console.log(" Job start as " + new Date().toLocaleString());
-//       await db.validateTimeConnection();      
-//       syncronizaWtaNow();
-//     });
-//   } catch (err) {
-//     throw new Error(`Can't start agenda! Err: ${err.message}`);
-//   }
+  console.log("Fim da leitura as " + new Date().toLocaleString());
 
- }
+  //   try {
+  //     const time = 60*24; //tempo em minutos
+  //     const job = nodeSchedule.scheduleJob(`*/${time} * * * *`, async () => {
+  //       console.log(" Job start as " + new Date().toLocaleString());
+  //       await db.validateTimeConnection();
+  //       syncronizaWtaNow();
+  //     });
+  //   } catch (err) {
+  //     throw new Error(`Can't start agenda! Err: ${err.message}`);
+  //   }
+}
 
 module.exports = { init };
